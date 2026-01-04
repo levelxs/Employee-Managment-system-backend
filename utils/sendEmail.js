dd773b1525805c329b5645c15f29b01b1f778b4f
Laxman Deshpande <Shreedeshpande90@gmail.com>
2026-01-04 23:28:12 +0530

    Update sendEmail.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,20 +1,52 @@
 const nodemailer = require("nodemailer");
 
-const sendEmail = async (to, subject, html) => {
-    const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS,
-        },
-    });
+// const sendEmail = async (to, subject, html) => {
+//     const transporter = nodemailer.createTransport({
+//         service: "gmail",
+//         auth: {
+//             user: process.env.EMAIL_USER,
+//             pass: process.env.EMAIL_PASS,
+//         },
+//     });
 
-    await transporter.sendMail({
-        from: `"Support" <${process.env.EMAIL_USER}>`,
-        to,
-        subject,
-        html,
+//     await transporter.sendMail({
+//         from: `"Support" <${process.env.EMAIL_USER}>`,
+//         to,
+//         subject,
+//         html,
+//     });
+// };
+
+const sendEmail = async (to: string, subject: string, html: string) => {
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  const mailData = {
+    from: `"Support" <${process.env.EMAIL_USER}>`,
+    to,
+    subject,
+    html,
+  };
+  await new Promise((resolve, reject) => {
+    transporter.sendMail(mailData, (err, info) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(info);
+      }
     });
+  });
 };
+
 
 module.exports = sendEmail;
